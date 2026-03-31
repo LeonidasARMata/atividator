c2cfbc4a085dc2cbb82fab791b134633cc2693e7
LeonidasARMata <leonidas.armata@gmail.com>
2026-03-31 19:27:03 -0300

    altera api_url

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'https://SEU-SERVICO.onrender.com',
+  API_URL: 'https://atividator.onrender.com',
   // Em desenvolvimento, descomente:
-   API_URL: 'http://localhost:3001',
+  // API_URL: 'http://localhost:3001',
 };
